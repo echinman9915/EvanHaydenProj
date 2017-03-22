225789fcb7e0f32215cc0baa8e5eb63396430a7b
evan_chinman <evan_chinman@student.wayland.k12.ma.us>
2017-03-22 08:11:18 -0400

    did some set up stuff

--- src/Batman.java
+++ src/Batman.java
@@ -5,7 +5,33 @@ public class Batman extends Sprite{
 
     public Batman(){
         super(400,400,NORTH);
+        setPic("batman ak.png", NORTH);
+        setSpeed(this.getBoundingRectangle().height);
 
     }
 
+    @Override
+    public void update() {
+        super.update();
+
+//        try {
+//            // open the sound file as a Java input stream
+//            String hop = "res/hop.wav";
+//            InputStream in = new FileInputStream(hop);
+//
+//            // create an audiostream from the inputstream
+//            AudioStream audioStream = new AudioStream(in);
+//
+//            // play the audio clip with the audioplayer class
+//            AudioPlayer.player.start(audioStream);
+//        }catch(Exception e){
+//            e.printStackTrace();
+//            System.out.println("Error loading sound file.");
+//        }
+
+    }
 }
+
+
+
+
